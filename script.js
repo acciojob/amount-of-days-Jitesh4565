b4141d7a2891a5a7641082145d842cd82468382a
Jitesh4565 <73875204+Jitesh4565@users.noreply.github.com>
2023-11-20 16:57:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function daysOfAYear(year){
+	if((year%4===0 && year%100!==0)||(year%400===0)){
+		return 366;
+	}
+	else{
+		return 365;
+	}
+}
+let input=prompt("Enter a Year");
+let userYear=parseInt(input);
+let numberOfDays=daysOfAYear(userYear);
